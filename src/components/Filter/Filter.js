12e90ec409c4fb30e0ce197b903f2d401cd4d395
Luis Manuel Meléndez Rodriguez <luismelendez0894@gmail.com>
2022-05-20 01:18:48 -0500

    navbar-commit

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './filter.css'
+
+const Filter = () => {
+  return (
+    <div>Filter</div>
+  )
+}
+
+export default Filter
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="container__navbar">
+        <div className="navbar__names">
+            <div className="" id="">
+            <div className="navbar__nav">
+                <a className="nav__link active" href="#">All</a>
+                <a className="nav__link" href="#">My faves</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
