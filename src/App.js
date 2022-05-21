cd309c5a9de5a05aabb5ab7499e9c8b3e3b0d5d9
Luis Manuel Meléndez Rodriguez <luismelendez0894@gmail.com>
2022-05-20 19:08:31 -0500

    api-commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Filter from './components/Filter/Filter';
 import Card from './components/Card/Card';
 
 function App() {
+
+  //Estado que recibe y actualiza los datos traidos de la API
+  const [newData, setNewData] = useState([]);
+
+  //Destructuración del objeto newData
+  const {hits} = newData;
+
+  //Guardamos la API
+  const API = "https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0";
+
+  useEffect(() => {
+    (async function () { 
+      const data = await fetch(API) //Hace el llamado a la API en un tiempo
+        .then((res) => res.json())  //Devolvemos la respuesta que recibimos en formato Json
+        setNewData(data) //Guardamos esa respuesta en setNewData
+     })();
+  },[API]);
+
+
   return (
     <>
       <Header />
       <Navbar />
       <Filter />
-      <Card />
+
+      <section className='container__card'>
+        <Card 
+        
+          hits={hits}/>
+      </section>
     </>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,173 +4,73 @@ import Time from '../../assets/img/time-icon.png'
 import HeartComplete from '../../assets/img/heart-com.png'
 import HeartIncomplete from '../../assets/img/heart-inc.png'
 
-const Card = () => {
+const Card = ({ hits }) => {
+
+ const handleSubmit = (e) => {
+     e.preventDefault();
+
+     /* const heart = document.querySelector('.card__right--icon');
+     heart.querySelector('img').setAttribute('src', HeartComplete); */
+     
+ };
+
+/* const paintHeart = e => {
+    e.preventDefault();
+    const heart = document.querySelector('.card__right--icon');
+
+    e.hits.forEach(item => {
+        heart.querySelector('img').setAttribute('src', item.HeartComplete);
+    });
+}
+ */
+
+  let showCard;
+
+  if(hits) {
+
+    showCard = hits.map((item) => {
+
+
+        let {author, story_title, story_url, created_at} = item;
+
+        return (
+                <a  href={story_url} target='_blank' key={`${author}-${created_at}`}>
+                    <div className='card__body'>
+                        <div className='card__left'>
+                                <div className='card__left--time'>
+                                    <img src={Time}/>
+                                    <span>
+                                        4 hours ago by {author === "" ? "Unknown" : author}
+                                    </span>
+                                </div>
+                                <span className='card__left--description'>
+                                {story_title === "" ? "Unknown" : story_title}
+                                </span>
+                        </div>
+ 
+                        <div className='card__right'>
+                            <div className='card__right--bg'></div>
+                                <div className='card__right--icon'>
+                                        <img 
+                                            src={HeartIncomplete}
+                                            onClick={handleSubmit} 
+                                        />
+                                </div>
+                        </div>
+                </div>
+            </a>
+          )
+    });
+
+  }
+
   return (
-    <section className='container__card'>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        4 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                Realize for React for Visualizing State ﬂow and component hierarchy.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartComplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        3 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                    Yes, React is taking over front-end development. The question is why.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartIncomplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        4 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                Realize for React for Visualizing State ﬂow and component hierarchy.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartComplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        3 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                    Yes, React is taking over front-end development. The question is why.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartIncomplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        4 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                Realize for React for Visualizing State ﬂow and component hierarchy.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartComplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        3 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                    Yes, React is taking over front-end development. The question is why.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartIncomplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        4 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                Realize for React for Visualizing State ﬂow and component hierarchy.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartComplete} />
-                </div>
-            </div>
-        </div>
-
-        <div className='card__body'>
-            <div className='card__left'>
-                <div className='card__left--time'>
-                    <img src={Time}/>
-                    <span>
-                        3 hours ago by author
-                    </span>
-                </div>
-                <span className='card__left--description'>
-                    Yes, React is taking over front-end development. The question is why.
-                </span>
-            </div>
-            <div className='card__right'>
-                <div className='card__right--bg'></div>
-                <div className='card__right--icon'>
-                    <img src={HeartIncomplete} />
-                </div>
-            </div>
-        </div>
-
-        
-    </section>
-  )
+    <> 
+      {/* Mostramos la variable */}
+      {showCard} 
+    </>
+    )
+  
 }
 
 export default Card
